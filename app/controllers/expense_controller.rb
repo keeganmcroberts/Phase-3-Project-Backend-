ff9969c568809b04cb8c1bb69887458f16a4bd6a
Caroline <ckmcdon@gmail.com>
2022-08-03 14:40:42 -0400

    updates to expense controller

--- app/controllers/expense_controller.rb
+++ app/controllers/expense_controller.rb
@@ -5,4 +5,20 @@ class ExpenseController < ApplicationController
     # { we_in: "AMAZING!" }.to_json
 
   end
+
+  post "/expenses" do
+    Expense.create(
+      amount: params[:amount],
+      # we don't know the category id, only the category name, 
+      category_id: params[:category_id],
+      date: params[:date],
+      # need it to be associated with currentUser 
+      # user_id: params[:currentUser.id]
+    )
+
+  delete "/expenses" do 
+    
+
+  end
+
 end
